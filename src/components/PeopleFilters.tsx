ce8e78098f21053a8ffe627ce904e2dbd0f8bdb9
Rodrigo <sistsayao@gmail.com>
2026-07-27 16:05:46 -0300

    Update PeopleFilters.tsx

--- src/components/PeopleFilters.tsx
+++ src/components/PeopleFilters.tsx
@@ -1,96 +1,48 @@
+import { useSearchParams } from 'react-router-dom';
+
 export const PeopleFilters = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get('query') || '';
+  const centuries = searchParams.getAll('centuries');
+
+  const handleQueryChange = e => {
+    const value = e.target.value;
+    const newParams = new URLSearchParams(searchParams);
+
+    if (value) {
+      newParams.set('query', value);
+    } else {
+      newParams.delete('query');
+    }
+
+    setSearchParams(newParams);
+  }; // <-- faltando isso aqui!
+
+  const handleCenturyClick = century => {
+    const newCenturies = centuries.includes(century)
+      ? centuries.filter(c => c !== century)
+      : [...centuries, century];
+    const newParams = new URLSearchParams(searchParams);
+
+    if (newCenturies.length > 0) {
+      newParams.set('centuries', newCenturies);
+    } else {
+      newParams.delete('centuries');
+    }
+
+    setSearchParams(newParams);
+  };
+
   return (
-    <nav className="panel">
-      <p className="panel-heading">Filters</p>
-
-      <p className="panel-tabs" data-cy="SexFilter">
-        <a className="is-active" href="#/people">
-          All
-        </a>
-        <a className="" href="#/people?sex=m">
-          Male
-        </a>
-        <a className="" href="#/people?sex=f">
-          Female
-        </a>
-      </p>
-
-      <div className="panel-block">
-        <p className="control has-icons-left">
-          <input
-            data-cy="NameFilter"
-            type="search"
-            className="input"
-            placeholder="Search"
-          />
-
-          <span className="icon is-left">
-            <i className="fas fa-search" aria-hidden="true" />
-          </span>
-        </p>
-      </div>
-
-      <div className="panel-block">
-        <div className="level is-flex-grow-1 is-mobile" data-cy="CenturyFilter">
-          <div className="level-left">
-            <a
-              data-cy="century"
-              className="button mr-1"
-              href="#/people?centuries=16"
-            >
-              16
-            </a>
-
-            <a
-              data-cy="century"
-              className="button mr-1 is-info"
-              href="#/people?centuries=17"
-            >
-              17
-            </a>
-
-            <a
-              data-cy="century"
-              className="button mr-1 is-info"
-              href="#/people?centuries=18"
-            >
-              18
-            </a>
-
-            <a
-              data-cy="century"
-              className="button mr-1 is-info"
-              href="#/people?centuries=19"
-            >
-              19
-            </a>
-
-            <a
-              data-cy="century"
-              className="button mr-1"
-              href="#/people?centuries=20"
-            >
-              20
-            </a>
-          </div>
-
-          <div className="level-right ml-4">
-            <a
-              data-cy="centuryALL"
-              className="button is-success is-outlined"
-              href="#/people"
-            >
-              All
-            </a>
-          </div>
-        </div>
-      </div>
-
-      <div className="panel-block">
-        <a className="button is-link is-outlined is-fullwidth" href="#/people">
-          Reset all filters
-        </a>
-      </div>
-    </nav>
+    <input
+      data-cy="NameFilter"
+      type="search"
+      className="input"
+      placeholder="Search"
+      value={query}
+      onChange={handleQueryChange}
+    />
+    // ...
   );
 };
+
